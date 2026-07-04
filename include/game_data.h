452698f456bbbec885db829bdc7fca11820d0a9d
Eclipse* <162909227+EclipseZPLAGUE@users.noreply.github.com>
2026-07-04 14:47:25 -0300

    Update game_data.h
    adding support for entities in the game_data file

--- include/game_data.h
+++ include/game_data.h
@@ -8,12 +8,16 @@ public:
 
 	void Init();
 	void SetMaxClients(int num)				{ MaxClients = num; }
+	void SetMaxEntities(int num)			{ MaxEntities = num; }
 	void SetMaxClientEdict(edict_t *pMax)	{ m_pMaxClientEdict = pMax; }
+	void SetMaxEntityEdict(edict_t *pMax)	{ m_pMaxEntityEdict = pMax; }
 	void SetStartEdict(edict_t *pStart)		{ m_pStartEdict = pStart; }
 	void SetStartEnt(edict_t *pEnt)			{ m_pStartEnt = pEnt; }
 
 	int GetMaxClients() const				{ return MaxClients; }
+	int GetMaxEntities() const				{ return MaxEntities; }
 	edict_t *GetMaxClientEdict() const		{ return m_pMaxClientEdict; }
+	edict_t *GetMaxEntityEdict() const		{ return m_pMaxEntityEdict; }
 	edict_t *GetStartEnt() const			{ return m_pStartEnt; }
 	edict_t *GetStartEdict() const			{ return m_pStartEdict; }
 
@@ -22,7 +26,9 @@ private:
 	edict_t *m_pStartEdict;
 	edict_t *m_pStartEnt;
 	edict_t *m_pMaxClientEdict;
+	edict_t *m_pMaxEntityEdict;
 	int MaxClients;
+	int MaxEntities;
 
 };
 
